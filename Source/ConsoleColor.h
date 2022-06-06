4430966a4a71f597f5ef90cb68b88dc10f5c6ff9
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-06-05 22:27:12 -0400

    Added ConsoleColor library component

---
+++ Source/ConsoleColor.h
@@ -0,0 +1,24 @@
+#define BLACK "\e[0;30m"	//dark color variants
+#define RED "\e[0;31m"		//
+#define GREEN "\e[0;32m"	//
+#define YELLOW "\e[0;33m"	//
+#define BLUE "\e[0;34m"		//
+#define MAGENTA "\e[0;35m"	//
+#define CYAN "\e[0;36m"		//
+#define WHITE "\e[0;37m"	//
+
+#define BBLACK "\e[1;30m"	//bright color variants
+#define BRED "\e[1;31m"		//
+#define BGREEN "\e[1;32m"	//
+#define BYELLOW "\e[1;33m"	//
+#define BBLUE "\e[1;34m"	//
+#define BMAGENTA "\e[1;35m"	//
+#define BCYAN "\e[1;36m"	//
+#define BWHITE "\e[1;37m"	//
+				
+#ifndef CONSOLECOLOR_H_
+#define CONSOLECOLOR_H_
+
+void resetColor();
+
+#endif
