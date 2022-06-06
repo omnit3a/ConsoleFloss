bb53a43ef99d8fc63bde65d656ed340214bd2213
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-06-05 22:27:44 -0400

    Updated build scripts to include reference to ConsoleColor

--- Test-Cases.bat
+++ Test-Cases.bat
@@ -1,7 +1,7 @@
 @echo OFF
 cd Build
 xcopy ..\Source /e
-gcc -o test-app.exe test-cases.c ConsoleFrame.c
+gcc -o test-app.exe test-cases.c ConsoleFrame.c ConsoleColor.c
 start test-app.exe
-del /f ConsoleFrame.c, ConsoleFrame.h
+del /f ConsoleFrame.c, ConsoleFrame.h, ConsoleColor.c, ConsoleColor.h
 pause
--- Test-Cases.sh
+++ Test-Cases.sh
@@ -2,6 +2,6 @@
 
 cp -p -r Source/. Build
 cd Build
-gcc -o test-app test-cases.c ConsoleFrame.c
+gcc -o test-app test-cases.c ConsoleFrame.c ConsoleColor.c
 ./test-app
-rm ConsoleFrame.c ConsoleFrame.h
+rm ConsoleFrame.c ConsoleFrame.h ConsoleColor.c ConsoleColor.h
