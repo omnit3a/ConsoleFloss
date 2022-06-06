41c67158dbe66aed3fa9bd210a22ea56e9de66ad
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-06-06 10:23:18 -0400

    Slightly change names of color constants

--- Source/ConsoleColor.h
+++ Source/ConsoleColor.h
@@ -7,14 +7,14 @@
 #define CYAN "\e[0;36m"		//
 #define WHITE "\e[0;37m"	//
 
-#define BBLACK "\e[1;30m"	//bright color variants
-#define BRED "\e[1;31m"		//
-#define BGREEN "\e[1;32m"	//
-#define BYELLOW "\e[1;33m"	//
-#define BBLUE "\e[1;34m"	//
-#define BMAGENTA "\e[1;35m"	//
-#define BCYAN "\e[1;36m"	//
-#define BWHITE "\e[1;37m"	//
+#define B_BLACK "\e[1;30m"	//bright color variants
+#define B_RED "\e[1;31m"	//
+#define B_GREEN "\e[1;32m"	//
+#define B_YELLOW "\e[1;33m"	//
+#define B_BLUE "\e[1;34m"	//
+#define B_MAGENTA "\e[1;35m"	//
+#define B_CYAN "\e[1;36m"	//
+#define B_WHITE "\e[1;37m"	//
 				
 #ifndef CONSOLECOLOR_H_
 #define CONSOLECOLOR_H_
